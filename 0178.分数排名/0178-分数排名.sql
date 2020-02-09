f35055ae235349ee98e0367f57ba1297cfaa3108
Incode <1206751678@qq.com>
2020-02-09 20:52:46 +0800

    2020-02-09

---
+++ 0178.分数排名/0178-分数排名.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT s1.Score, COUNT(DISTINCT(s2.Score)) AS 'RANK'
+FROM Scores s1, Scores s2
+WHERE s1.Score <= s2.Score
+GROUP BY s1.Id
+ORDER BY RANK
