ad47b0a691dbe3859c4ee15c4dab3e88629bf3ac
Incode <1206751678@qq.com>
2020-02-10 20:57:23 +0800

    2020-02-10

---
+++ 0184.部门工资最高的员工/0184-部门工资最高的员工.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+-- 先找出每个部门的 max，接着用 in 匹配
+select 
+    d.Name AS 'Department',
+    e.Name AS 'Employee',
+    Salary
+from 
+    Employee e left join Department d on e.DepartmentId = d.Id
+where 
+(d.Id, e.Salary) in (
+    select DepartmentId, max(Salary)
+    from Employee
+    group by DepartmentId
+)
